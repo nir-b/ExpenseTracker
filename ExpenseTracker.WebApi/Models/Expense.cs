6a1033e6aedb14221fdf9d0f49cec85085d6fb79
Nirmalya Bhattacharyya <saanvithegeek@gmail.com>
2021-11-16 08:08:28 +0100

    Added missing required attributes

--- ExpenseTracker.WebApi/Models/Expense.cs
+++ ExpenseTracker.WebApi/Models/Expense.cs
@@ -6,18 +6,22 @@ namespace ExpenseTracker.WebApi.Models
 {
     public class Expense
     {
-        [Key] public int Id { get; set; }
-
-        [StringLength(512)] public string Title { get; set; }
-
-        [StringLength(128)] public string Category { get; set; }
-
+        [Key] 
+        public int Id { get; set; }
+        [Required]
+        [StringLength(512)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(128)] 
+        public string Category { get; set; }
+        [Required]
         public DateTime ExpenseDate { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(8, 2)")] 
+        public double Amount { get; set; }
 
-        [Column(TypeName = "decimal(8, 2)")] public double Amount { get; set; }
-
-        [StringLength(1024)] public string Description { get; set; }
-
+        [StringLength(1024)] 
+        public string Description { get; set; }
         public DateTime DateAdded { get; set; }
         public DateTime DateUpdated { get; set; }
         public DateTime? DateDeleted { get; set; }
